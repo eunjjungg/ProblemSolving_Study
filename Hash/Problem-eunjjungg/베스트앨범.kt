bb024f74d70a73a421390ec6452c81f267f4d915
EunJung Jung <100047095+eunjjungg@users.noreply.github.com>
2023-04-24 17:28:44 +0900

    Update 베스트앨범.kt

--- Hash/Problem-eunjjungg/베스트앨범.kt
+++ Hash/Problem-eunjjungg/베스트앨범.kt
@@ -14,8 +14,48 @@
 class P42579 {
     fun solution(genres: Array<String>, plays: IntArray): IntArray {
         var answer = mutableListOf<Int>()
+        var hash = HashMap<String, Int>()
+
+        genres.forEachIndexed { index, genre ->
+            hash.put(genre, hash.get(genre)?.plus(plays[index]) ?: plays[index])
+        }
+        val sortedGenres = hash.toList().sortedByDescending { it.second } // hash가 많이 들은 장르 순으로 정렬됨
+
+        sortedGenres.forEach { genreMap ->
+            var max_i = -1
+            var max_p = -1
+
+            genres.forEachIndexed { index, genre ->
+                if (genre == genreMap.first && index != max_i && max_p < plays[index]) {
+                    max_i = index
+                    max_p = plays[index]
+                }
+            }
+            answer.add(max_i)
+
+            max_p = -1
+            genres.forEachIndexed { index, genre ->
+                if (genre == genreMap.first && index != answer.last() && max_p <= plays[index]) {
+                    if (max_p == plays[index] && index < max_i) {
+                        max_i = index
+                    } else if (max_p != plays[index]) {
+                        max_i = index
+                    }
+                    max_p = plays[index]
+                }
+            }
+            if (max_i != answer.last()) {
+                answer.add(max_i)
+            }
+        }
+
+
         return answer.toIntArray()
     }
 }
 
 // Review
+// 코드는 복잡하지만 맞게 푼 것 같은데 계속 오류가 났다.
+// 왜 그랬냐면 map, hashmap을 그냥 sortedMap 메소드 사용을 하면
+// key 값을 기준으로 정렬이 됨. 하지만 난 value 기준으로 정렬을 해야 하므로
+// list로 변환 후 sortedByDescending { it.second }로 해주어야 함.
