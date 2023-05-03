9d4082100343385ec0e32d3ab7f6e26360e6edbd
EunJung Jung <100047095+eunjjungg@users.noreply.github.com>
2023-05-03 23:26:11 +0900

    Create 기능개발.kt

---
+++ Stack_Queue/Problem-eunjjungg/기능개발.kt
@@ -0,0 +1,16 @@
+// Strategy
+// 100 - progress[i] = x
+// ceil(x / speed[i]) = n
+// n을 queue에 추가
+// queue의 element를 pivot으로 정하고 pop() 해준 뒤
+// 다음 element가 pivot 이하일 때 pop() 해주고 count++를 해서 
+// ans 배열에 넣어줌. 이후 count 초기화
+
+class P42586 {
+    fun solution(progresses: IntArray, speeds: IntArray): IntArray {
+        var answer = intArrayOf()
+        return answer
+    }
+}
+
+// Review
