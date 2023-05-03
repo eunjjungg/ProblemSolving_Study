79d9db11dacfaa28cea39a3d56c6f8ceee783ad8
EunJung Jung <100047095+eunjjungg@users.noreply.github.com>
2023-05-03 23:49:01 +0900

    Create 프로세스.kt

---
+++ Stack_Queue/Problem-eunjjungg/프로세스.kt
@@ -0,0 +1,16 @@
+// Strategy
+// 우선순위 기준으로 정렬
+// 우선순위가 4, 3, 2, 1, 0 순으로 있을 때
+// priority가 4인 것의 가장 뒤에 있는 거 찾고 해당 인덱스보다 앞에 있는 priority들 다 배열뒤로 붙임.
+// 그리고 priority가 4인 것의 Location들 pop해줌. count도 pop된 개수만큼 ++를 해줌. 
+// 3, 2, 1, 0에 대해서도 반복.
+// 이 과정 수행하면서 locations queue에서 pop 된게 문제에서 주어진 Location일 때 count의 개수 Return 
+
+class P42587 {
+    fun solution(priorities: IntArray, location: Int): Int {
+        var answer = 0
+        return answer
+    }
+}
+
+// Review
