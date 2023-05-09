16a6f364b3bb8c4248737ddec8a2d250bdee41d2
EunJung Jung <100047095+eunjjungg@users.noreply.github.com>
2023-05-09 21:31:37 +0900

    Create 뒤에 있는 큰 수 찾기.kt

---
+++ Bonus/eunjjungg/뒤에 있는 큰 수 찾기.kt
@@ -0,0 +1,13 @@
+// Strategy
+// 가장 쉬운건 이중포문 -> 그러나 효율 안 좋음
+// 배열 맨 뒤부터 stack에 넣고 하나씩 꺼내서 남은 원소에 대해 비교하며
+// 자기 자신보다 뒤면서 큰 값이 있다면 answer에 넣어주고 없다면 -1을 넣어줌
+class P154539 {
+    fun solution(numbers: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        return answer
+    }
+}
+
+// Review
+// 리뷰에 대한 설명 (다음 주차에 작성)
